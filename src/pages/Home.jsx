74be855f577fbe8bd601a67d1c4ed2c6f326a7f5
Sumit Kumar <developer.sumit.professional@gmail.com>
2026-04-16 07:13:38 +0530

    Refactor WelcomeIntro rendering for improved readability

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -42,9 +42,7 @@ const Home = () => {
 
   return (
     <div className="md:px-24 relative">
-      {showWelcomeIntro && (
-        <WelcomeIntro onComplete={handleWelcomeComplete} />
-      )}
+      {showWelcomeIntro && <WelcomeIntro onComplete={handleWelcomeComplete} />}
       <Hero />
       <div className="w-full h-[75px] border-l border-r border-white/20 bg-[radial-gradient(circle,_#1D202A_1px,_transparent_1px)] [background-size:20px_18px] bg-[#000000]"></div>
       <Projects />
